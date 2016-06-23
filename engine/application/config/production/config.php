f6966acdf330995e1eb36102dcba1aeae5e0ca6c
marwansaleh <marwansaleh@192.168.1.101>
2016-06-23 20:06:54 +0700

    Update

--- engine/application/config/production/config.php
+++ engine/application/config/production/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://172.18.2.47:8282/';
+$config['base_url'] = 'http://doc.bappeda-lubuklinggaukota.net/';
 
 /*
 |--------------------------------------------------------------------------
