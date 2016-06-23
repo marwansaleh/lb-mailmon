72b3b4e9f465a0a087bb8889b8d1904c4110d1d5
marwansaleh <marwansaleh@172.18.1.216>
2016-06-23 16:29:46 +0700

    Update

--- engine/application/libraries/Service.php
+++ engine/application/libraries/Service.php
@@ -6,31 +6,20 @@
  * @author marwansaleh 11:13:40 PM
  */
 class Service extends Library {
-    private $_service_url;
-    private $_temp_path = 'tmp/';
-    
+    private $ci;
     function __construct() {
         parent::__construct();
-        
-        
-        $this->_service_url = $this->ci->config->item('service_url');
+        $this->ci =& get_instance(); 
     }
     
     public function get_users($array=FALSE){
-        $temp_name = $this->_temp_path . 'user_services.json';
-        
         $users = NULL;
-        if (file_exists($temp_name)){
-            $users = json_decode(file_get_contents($temp_name));
-        }else{
-            $user_service = $this->_get_users_service();
-            if ($user_service){
-                file_put_contents($temp_name, json_encode($user_service));
-                
-                $users = $user_service;
-            }
+        
+        if (!isset($this->ci->user_m)){
+            $this->ci->load->model('user_m');
         }
         
+        $users = $this->ci->user_m->get();
         
         if ($users){
             if ($array){
@@ -46,46 +35,12 @@ class Service extends Library {
         return $users;
     }
     
-    protected function _get_users_service($simple=0){
-        $service_url = config_item('service_url') . 'user/all/'.$simple;
-        
-        $ch = curl_init(); 
-        // set url 
-        curl_setopt($ch, CURLOPT_URL, $service_url); 
-
-        curl_setopt($ch,CURLOPT_POST,FALSE); 
-        //return the transfer as a string 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-
-        // $output contains the output string 
-        $output = curl_exec($ch); 
-        if (curl_errno($ch)){
-            return FALSE;
-        }else{
-            //echo $output; exit;
-            $output = json_decode($output);
-        }
-        // close curl resource to free up system resources 
-        curl_close($ch);  
-        
-        return $output->items;
-    }
-    
     public function get_bidang($array=FALSE){
-        $temp_name = $this->_temp_path . 'bidang_services.json';
-        
-        $items = NULL;
-        if (file_exists($temp_name)){
-            $items = json_decode(file_get_contents($temp_name));
-        }else{
-            $service = $this->_get_bidang_service();
-            if ($service){
-                file_put_contents($temp_name, json_encode($service));
-                
-                $items = $service;
-            }
+        if (!isset($this->ci->bidang_m)){
+            $this->ci->load->model('bidang_m');
         }
         
+        $items = $this->ci->bidang_m->get();
         
         if ($items){
             if ($array){
@@ -99,48 +54,14 @@ class Service extends Library {
         }
         
         return $items;
-    }
-    
-    protected function _get_bidang_service(){
-        $service_url = config_item('service_url') . 'bidang/all';
-        
-        $ch = curl_init(); 
-        // set url 
-        curl_setopt($ch, CURLOPT_URL, $service_url); 
-
-        curl_setopt($ch,CURLOPT_POST,FALSE); 
-        //return the transfer as a string 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-
-        // $output contains the output string 
-        $output = curl_exec($ch); 
-        if (curl_errno($ch)){
-            return FALSE;
-        }else{
-            //echo $output; exit;
-            $output = json_decode($output);
-        }
-        // close curl resource to free up system resources 
-        curl_close($ch);  
-        
-        return $output->items;
     }
     
     public function get_groups($array=FALSE){
-        $temp_name = $this->_temp_path . 'groups_services.json';
-        
-        $items = NULL;
-        if (file_exists($temp_name)){
-            $items = json_decode(file_get_contents($temp_name));
-        }else{
-            $service = $this->_get_group_service();
-            if ($service){
-                file_put_contents($temp_name, json_encode($service));
-                
-                $items = $service;
-            }
+       if (!isset($this->ci->group_m)){
+            $this->ci->load->model('group_m');
         }
         
+        $items = $this->ci->group_m->get();
         
         if ($items){
             if ($array){
@@ -154,112 +75,6 @@ class Service extends Library {
         }
         
         return $items;
-    }
-    
-    protected function _get_group_service(){
-        $service_url = config_item('service_url') . 'grup/all';
-        
-        $ch = curl_init(); 
-        // set url 
-        curl_setopt($ch, CURLOPT_URL, $service_url); 
-
-        curl_setopt($ch,CURLOPT_POST,FALSE); 
-        //return the transfer as a string 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-
-        // $output contains the output string 
-        $output = curl_exec($ch); 
-        if (curl_errno($ch)){
-            return FALSE;
-        }else{
-            //echo $output; exit;
-            $output = json_decode($output);
-        }
-        // close curl resource to free up system resources 
-        curl_close($ch);  
-        
-        return $output->items;
-    }
-    
-    public function get_wilayah($array=FALSE){
-        $temp_name = $this->_temp_path . 'wilayah_services.json';
-        
-        $items = NULL;
-        if (file_exists($temp_name)){
-            $items = json_decode(file_get_contents($temp_name));
-        }else{
-            $service = $this->_get_wilayah_service();
-            if ($service){
-                file_put_contents($temp_name, json_encode($service));
-                
-                $items = $service;
-            }
-        }
-        
-        
-        if ($items){
-            if ($array){
-                $result = array();
-                foreach ($items as $item){
-                    $result[$item->id] = $item;
-                }
-            }
-            
-            return $result;
-        }
-        
-        return $items;
-    }
-    
-    protected function _get_wilayah_service(){
-        $service_url = config_item('service_url') . 'wilayah/all';
-        
-        $ch = curl_init(); 
-        // set url 
-        curl_setopt($ch, CURLOPT_URL, $service_url); 
-
-        curl_setopt($ch,CURLOPT_POST,FALSE); 
-        //return the transfer as a string 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-
-        // $output contains the output string 
-        $output = curl_exec($ch); 
-        if (curl_errno($ch)){
-            return FALSE;
-        }else{
-            //echo $output; exit;
-            $output = json_decode($output);
-        }
-        // close curl resource to free up system resources 
-        curl_close($ch);  
-        
-        return $output->items;
-    }
-    
-    public function generate_nomor($parameters){
-        $service_url = $this->ci->config->item('service_nomor_url');
-        
-        $ch = curl_init(); 
-        // set url 
-        curl_setopt($ch, CURLOPT_URL, $service_url); 
-
-        curl_setopt($ch,CURLOPT_POST,TRUE); 
-        curl_setopt($ch,CURLOPT_POSTFIELDS,  http_build_query($parameters));
-        //return the transfer as a string 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-
-        // $output contains the output string 
-        $output = curl_exec($ch); 
-        if (curl_errno($ch)){
-            return FALSE;
-        }else{
-            //echo $output; exit;
-            $output = json_decode($output);
-        }
-        // close curl resource to free up system resources 
-        curl_close($ch);  
-        
-        return $output;
     }
 }
 
