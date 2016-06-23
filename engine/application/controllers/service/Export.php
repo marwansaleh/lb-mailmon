d27423da8a185ad6c2c116e155cd50ce12acc76e
marwansaleh <marwansaleh@192.168.1.101>
2016-06-23 21:48:23 +0700

    Update

--- engine/application/controllers/service/Export.php
+++ engine/application/controllers/service/Export.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-require_once  APPPATH.'libraries/PHPWord/Autoloader.php';
+require_once  APPPATH.'libraries/PhpWord/Autoloader.php';
 
 /**
  * Description of Export
